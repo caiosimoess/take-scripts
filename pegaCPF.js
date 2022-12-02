a0cf091bce4b0a4abfb5d2752828362d6d325d6d
Caio Simoes <caiocesarsimoes@gmail.com>
2022-12-02 13:45:32 -0300

    Commit pegaCNPJ.js, updated pegaCPF.js

--- pegaCPF.js
+++ pegaCPF.js
@@ -1,5 +1,4 @@
 // Extração dos números do cpf em formato string
-
 function strCPF(inputUsuario) {
 
   var regex = /[0-9]{3}[\.\ \-]?[0-9]{3}[\.\ \-]?[0-9]{3}[\.\ \-]?[0-9]{2}/;
@@ -11,25 +10,26 @@ function strCPF(inputUsuario) {
 
 
 // função TestaCPF está disponível no site da Receita Federal 
-
-function TestaCPF(strCPF) {
-
+function TestaCPF(inputUsuario) {
+	
+  cpf = strCPF(inputUsuario);
+  
   var Soma;
   var Resto;
   Soma = 0;
-  if (strCPF == "00000000000") return false;
+  if (cpf == "00000000000") return false;
 
-  for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+  for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
   Resto = (Soma * 10) % 11;
 
   if ((Resto == 10) || (Resto == 11)) Resto = 0;
-  if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+  if (Resto != parseInt(cpf.substring(9, 10))) return false;
 
   Soma = 0;
-  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
   Resto = (Soma * 10) % 11;
 
   if ((Resto == 10) || (Resto == 11)) Resto = 0;
-  if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+  if (Resto != parseInt(cpf.substring(10, 11))) return false;
   return true;
 }
